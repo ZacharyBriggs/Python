8dd361a7df0f8b8cdf682ae5cfcfb07487577108
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-02-05 15:38:51 -0600

    letters can be uppercased + adding strings fixed

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -4,11 +4,14 @@ from random import *
 def main():
     words = open("words.txt", 'r')
     password = open("passwords.txt", 'a')
-    rand = randint(1, 76)
-    rand_two = randint(1,76)
+    special_chars = open("special_chars.txt", 'r')
+    rand = randint(0, 76)
+    rand_two = randint(0, 76)
+    rand_spec_char = randint(0, 10)
     iterator = 0
     strength = 0
-    strength = input("Choose your password strength. 0-5\n")
+    #strength = input("Choose your password strength. 0-5\n")
+    strength = 3
     #Outputs a random word
     if strength is 0:
         for rand_word in words:
@@ -31,8 +34,11 @@ def main():
         for rand_word in words:
             iterator += 1
             if rand is iterator:
-                rand_num = randint(0,100)
+                rand_num = randint(0, 100)
+                #for
+                rand_word = rand_word.replace("\n", "")
                 rand_word += str(rand_num)
+                rand_word += "\n"
                 password.write(rand_word)
                 password.write("\n")
     #Outputs 2 randomwords + a random number + a special character + first letter is uppercased
@@ -44,15 +50,18 @@ def main():
                 for rand_word_two in words:
                     iterator += 1
                     if rand_two is iterator:
-                        if int(rand_word[0]) > 96:
-                            rand_word[0] -= 32
-                        rand_num = randint(0,100)
+                        rand_word = rand_word.replace("\n", "")
+                        rand_word_two = rand_word.replace("\n", "")
+                        rand_word = rand_word.replace(rand_word[0], chr(ord(rand_word[0]) - 32))
+                        rand_num = randint(0, 100)
                         rand_word += rand_word_two
                         rand_word += str(rand_num)
+                        rand_word += "\n"
                         print rand_word
                         password.write(rand_word)
                         password.write("\n")
-    #Outputs 2 randomwords + a random number + a special character + all convertable letters are changed into a special character
+    #Outputs 2 randomwords + a random number + a special character + first letter is uppercased
+    #+ all convertable letters are changed into a special character
     elif strength is 4:
         for rand_word in words:
             iterator += 1
@@ -61,15 +70,19 @@ def main():
                 for rand_word_two in words:
                     iterator += 1
                     if rand_two is iterator:
-                        if int(rand_word[0]) > 96:
-                            rand_word[0] -= 32
-                        rand_num = randint(0,100)
+                        rand_word = rand_word.replace("\n", "")
+                        rand_word_two = rand_word.replace("\n", "")
+                        rand_word = rand_word.replace(rand_word[0], chr(ord(rand_word[0]) - 32))
+                        rand_num = randint(0, 100)                 
                         rand_word += rand_word_two
                         rand_word += str(rand_num)
+                        rand_word += "\n"
                         print rand_word
                         password.write(rand_word)
                         password.write("\n")
-    #Outputs 2 random words + a random number + a special character + all convertable letters are changed into a special character + all convertable letters are changed into a number
+    #Outputs 2 random words + a random number + a special character + first letter is uppercased
+    #+ all convertable letters are changed into a special character
+    #+ all convertable letters are changed inASto a number
     elif strength is 5:
         print "none"
     else:
