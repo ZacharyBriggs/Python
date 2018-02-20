f35c1e1ec0784cdfe579ec99999e99835671f834
ZacharyBriggs <zacharybriggs64@gmail.com>
2018-02-20 09:54:04 -0600

    find_neighbors in graph.py broke out astar function

--- astar.py
+++ astar.py
@@ -10,60 +10,40 @@ class A_Star:
         #search space variable
 
     def startup(self, width, height):
-        search_area = Graph(Vector2(width, height))
-        search_area.create_nodes()
-        open_list = []
-        open_list.append(search_area.nodes[6])
-        close_list = []
-        parents = []
-        neighbors = []
-        goal_node = Vector2(3,3)
+        self.search_area = Graph(Vector2(width, height))
+        self.search_area.create_nodes()
+        self.open_list.append(self.search_area.nodes[6])
+        self.parents = []
+        self.neighbors = []
 
     def pathfind(self):
         goal_found = False
         while(goal_found == False):
-            current_node = open_list[0]
-            open_list.append(current_node)
-            close_list.append(current_node)
-            if close_list[len(close_list) - 1].position == goal_node:
+            current_node = self.open_list[0]
+            self.open_list.append(current_node)
+            self.close_list.append(current_node)
+            if self.close_list[len(self.close_list) - 1].position == self.goal_node:
                 goal_found = True
                 break
-            neighbors = find_neighbors(search_area, current_node.position)
-            for neighbor in neighbors:
-                open_list.append(neighbor)
-            for item in open_list:
+            self.neighbors = self.search_area.find_neighbors(current_node.position)
+            for neighbor in self.neighbors:
+                self.open_list.append(neighbor)
+            for item in self.open_list:
                 if item.traversable == True:
                     item.calc_g(current_node)
                     #if item.parent != None:
                         #if item.g_score < item.parent.g_score:
                             #item.parent = item
-                    item.calc_h(goal_node)
+                    item.calc_h(self.goal_node)
                     item.calc_f()
                     if item.g_score < current_node.g_score:
                         current_node = item
                 item.set_parent(current_node)
-        parents.append(current_node)
-        open_list.sort(key = lambda Node: Node.f_score)
-
-def find_neighbors(self, graph, current_pos):
-    neighbors = []
-    neighbor_pos = [] 
-    neighbor_pos.append(current_pos + Vector2(-1, 1)) #Top Left
-    neighbor_pos.append(current_pos + Vector2(0, 1)) #Top
-    neighbor_pos.append(current_pos + Vector2(1, 1)) #Top Right
-    neighbor_pos.append(current_pos + Vector2(-1, 0)) #Left
-    neighbor_pos.append(current_pos + Vector2(1, 0)) #Right
-    neighbor_pos.append(current_pos + Vector2(-1, -1)) #Bot Left
-    neighbor_pos.append(current_pos + Vector2(0, -1)) #Bot
-    neighbor_pos.append(current_pos + Vector2(1, -1)) #Bot Right
-    for node in graph.nodes:
-        for pos in neighbor_pos:
-            if node.position.x_pos == pos.x_pos and node.position.y_pos == pos.y_pos:
-                neighbors.append(node)
-    return neighbors
+            self.parents.append(current_node)
+            self.open_list.sort(key = lambda Node: Node.f_score)
 
 def main():
-    astar = A_Star(Vector2(1,1),Vector2(3,3))
-    astar.startup(5,5)
+    astar = A_Star(Vector2(2, 4),Vector2(6, 4))
+    astar.startup(7, 7)
     astar.pathfind()
 main()
